ab53d68f6734c5f0f57fbc4c011ef3e5af76128c
saitoriahanipale <thanipale12@gmail.com>
2025-09-11 16:35:13 +1000

    Linked the OceaniaController to the TestPageController.

--- src/main/java/com/example/geoquestkidsexplorer/controllers/OceaniaController.java
+++ src/main/java/com/example/geoquestkidsexplorer/controllers/OceaniaController.java
@@ -25,9 +25,17 @@ public class OceaniaController {
     }
 
     @FXML
-    private void handleTest(ActionEvent event) {
-        System.out.println("User chose to test their knowledge of Oceania.");
-        // TODO: Navigate to Test page for Oceania
+    private void handleTest(ActionEvent event) throws IOException {
+        //System.out.println("User chose to test their knowledge of Oceania.");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/geoquestkidsexplorer/testpage.fxml"));
+        Parent root = loader.load();
+
+        TestPageController controller = loader.getController();
+        controller.setRegion("Oceania");
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
 
--- src/main/java/com/example/geoquestkidsexplorer/controllers/TestPageController.java
+++ src/main/java/com/example/geoquestkidsexplorer/controllers/TestPageController.java
@@ -6,15 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 
 
 public class TestPageController {
+    public void setRegion(String region) {
+        Label regionLabel = new Label();
+        regionLabel.setText("Let's learn about "+ region);
+    }
 
     private void handleBack(ActionEvent event){
         try {
             Parent root = FXMLLoader.load(
-                getClass().getResource("/com/geoquestkidsexplorer/homepage.fxml"));
+                getClass().getResource("/com/example/geoquestkidsexplorer/homepage.fxml"));
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root, stage.getScene().getWidth(), stage.getScene().getHeight()));
         stage.show();
