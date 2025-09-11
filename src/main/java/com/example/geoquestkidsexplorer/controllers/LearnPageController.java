75a81e4083864b7e93b329eb95437ad37434a9f1
Ava-Suresh- L <n12016063@qut.edu.au>
2025-09-11 20:11:02 +1000

    home bage for learning page

--- src/main/java/com/example/geoquestkidsexplorer/controllers/LearnPageController.java
+++ src/main/java/com/example/geoquestkidsexplorer/controllers/LearnPageController.java
@@ -2,6 +2,13 @@ package com.example.geoquestkidsexplorer.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class LearnPageController {
 
@@ -10,6 +17,16 @@ public class LearnPageController {
 
     public void setRegion(String region) {
         regionLabel.setText("Let's learn about "+ region);
+    }
+
+    @FXML
+    private void handleBackToHome(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/geoquestkidsexplorer/homepage.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
 
