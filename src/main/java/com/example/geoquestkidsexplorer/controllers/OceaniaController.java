86aef938bc5b41b409746e9c57aad261506f66d4
BigMummaHere79 <glenda/fulgidezza@connect.qut.edu.au>
2025-09-13 22:32:59 +1000

    To Nikki, i have link the quiz_view.fxml for "Oceania" continent to Test Mode Quiz in the OceaniaController.java. I have leave a comment for your references.

--- src/main/java/com/example/geoquestkidsexplorer/controllers/OceaniaController.java
+++ src/main/java/com/example/geoquestkidsexplorer/controllers/OceaniaController.java
@@ -1,5 +1,7 @@
 package com.example.geoquestkidsexplorer.controllers;
 
+import com.example.geoquestkidsexplorer.controllers.HomePageController;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
@@ -30,79 +32,59 @@ public class OceaniaController {
 
     @FXML
     private void handleGameModeClick(MouseEvent event) {
-        // Get the source of the click, which is the VBox tile
+        // Get the source of the click, which is the VBox tile.
         Node clickedTile = (Node) event.getSource();
         String tileId = clickedTile.getId();
 
-        // Use the ID to determine which game mode was selected
-        if ("practiceModeTile".equals(tileId)) {
-            try {
-                // Load the practice quiz FXML file
-                Parent root = FXMLLoader.load(getClass().getResource(
-                        "/com/example/geoquestkidsexplorer/practicequizoceania.fxml"));
-                Scene scene = new Scene(root);
-                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                stage.setScene(scene);
-                stage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
+        try {
+            // Use the ID to determine which game mode was selected and load the corresponding scene.
+            if ("practiceModeTile".equals(tileId)) {
+                loadScene("/com/example/geoquestkidsexplorer/practicequizoceania.fxml", event);
+            } else if ("testModeTile".equals(tileId)) {
+                System.out.println("Test Mode Quiz selected!");
+                //loadScene("/com/example/geoquestkidsexplorer/quiz_view.fxml", event);
+                openQuiz("Oceania");
             }
-        } else if ("testModeTile".equals(tileId)) {
-            System.out.println("Test Mode Quiz selected!");
-            // TODO: Implement logic to start the Test Mode Quiz
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
+    private void openQuiz(String continent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/com/example/geoquestkidsexplorer/quiz_view.fxml"));
+            Parent root = loader.load();
 
-    /*@FXML
-    private void handleLearn(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/geoquestkidsexplorer/LearnPage.fxml"));
-        Parent root = loader.load();
+            // Show quiz in a new window (keeps Home open)
+            Stage quizStage = new Stage();
+            quizStage.setTitle(continent + " Quiz");
+            quizStage.setScene(new Scene(root, 800, 600));
 
-        LearnPageController controller = loader.getController();
-        controller.setRegion("Oceania");
+            // Pass data into the quiz controller
+            QuizController controller = loader.getController();
+            controller.setStage(quizStage);       // so Back can close this window
+            controller.setContinent(continent);   // loads the first question
 
+            quizStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * A private helper method to load a new FXML scene and transition to it.
+     * This version is more flexible and can accept any type of Event.
+     *
+     * @param fxmlPath The path to the FXML file to load.
+     * @param event The event that triggered the action.
+     * @throws IOException If the FXML file cannot be loaded.
+     */
+    private void loadScene(String fxmlPath, Event event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(fxmlPath));
+        Scene scene = new Scene(root);
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(root));
+        stage.setScene(scene);
         stage.show();
-    }*/
-
-    /*@FXML
-    private void handleTest(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/geoquestkidsexplorer/testpage.fxml"));
-        Parent root = loader.load();
-
-        TestPageController controller = loader.getController();
-        controller.setRegion("Oceania");
-
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.getScene().setRoot(root);
-        stage.show();
-    }*/
-
-    /*@FXML
-    private void handleFlashcards(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/geoquestkidsexplorer/FlashcardsPage.fxml"));
-        Parent root = loader.load();
-
-        FlashcardsController controller = loader.getController();
-        controller.setRegion("Oceania");  // Pass the selected region dynamically here
-
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.getScene().setRoot(root);
-        stage.show();
-    }*/
-
-
-
-    /*@FXML
-    private void handleBackToHome(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/geoquestkidsexplorer/mainapp.fxml"));
-        Parent root = loader.load();
-
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.getScene().setRoot(root);
-        stage.show();
-    }*/
-
+    }
 }
