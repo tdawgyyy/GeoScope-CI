aae7bdcc8d988a2d5b9580eeec528b5f0c7cffd6
saitoriahanipale <thanipale12@gmail.com>
2025-09-17 01:16:37 +1000

    Adding test connection in Test > DatabaseTest

--- src/test/java/com/example/geoquestkidsexplorer/database/DatabaseTest.java
+++ src/test/java/com/example/geoquestkidsexplorer/database/DatabaseTest.java
@@ -1,7 +1,19 @@
 package com.example.geoquestkidsexplorer.database;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import com.example.geoquestkidsexplorer.database.DatabaseManager;
+import org.junit.jupiter.api.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DatabaseTest {
+
+    //Test Database Connection
+    @Test
+    public void testConnection() throws SQLException {
+        Connection conn = DatabaseManager.getConnection();
+        assertEquals(true, conn != null);
+    }
 }
